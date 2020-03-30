2b429fdeb63ccd9bba6be09c91c88de26dcbc1f1
mizchi <miz404@gmail.com>
2020-03-31 02:13:29 +0900

    Update

--- script/push-if-changed.bash
+++ script/push-if-changed.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+# TODO: Update only new src
 git diff --exit-code --quiet templates/src
-if [ 0 -ne $? ]; then
-  git config --global user.email "CI@github"
-  git config --global user.name "CI"
-  git add templates/gen
-  git commit -m "[CI] Gen templates/gen"
-  echo gen
+# if [ 0 -ne $? ]; then
+git config --global user.email "CI@github"
+git config --global user.name "CI"
+git add templates/gen
+git commit --allow-empty -m "[CI] Gen templates/gen"
   # git push origin master
-else
-  echo no changes
-fi
+# else
+#   echo no changes
+# fi
