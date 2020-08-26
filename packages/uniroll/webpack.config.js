8371c0a24d150b5dfb1ebec6bb049a236c31c798
mizchi <miz404@gmail.com>
2020-08-27 01:22:43 +0900

    Refactor

--- packages/uniroll/webpack.config.js
+++ packages/uniroll/webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   ...shared,
   entry: {
     "uniroll-transformer": path.join(__dirname, "src/transformer"),
-    // "uniroll-baseline": path.join(__dirname, "src/baseline"),
-    // "uniroll-prod": path.join(__dirname, "src/prod"),
+    "uniroll-baseline": path.join(__dirname, "src/baseline"),
+    "uniroll-prod": path.join(__dirname, "src/prod"),
     uniroll: path.join(__dirname, "src/index"),
   },
   output: {
