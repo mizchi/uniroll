269ab8e36291d3681f4348c1911a67ad5b562d9e
mizchi <miz404@gmail.com>
2020-08-31 22:45:34 +0900

    Fix

--- packages/uniroll/src/helpers.ts
+++ packages/uniroll/src/helpers.ts
@@ -1,44 +1,15 @@
+import { ImportMaps } from "rollup-plugin-http-resolve";
 import { UnirollConfigBuilderResult } from "./config/base";
 import { UnirollOptions } from "./config/base";
 import { rollup, RollupOptions } from "rollup";
 import type fs from "fs";
 import { Volume } from "memfs";
 import createFs, { IPromisesAPI } from "memfs/lib/promises";
-import { ImportMap } from "./types";
+// import { ImportMap } from "./types";
 
 export function createMemoryFs(files: { [k: string]: string }): IPromisesAPI {
   const vol = Volume.fromJSON(files, "/");
   return createFs(vol) as IPromisesAPI;
-}
-
-export function createUrlRewriter(opts: {
-  getImportMap?: () => Promise<ImportMap | void> | ImportMap | void;
-  onWarn?: (arg: any) => void;
-}) {
-  return async (id: string, importer: string | void = undefined) => {
-    // TODO: handle npm versions
-    if (importer == null) {
-      return;
-    }
-    if (id.startsWith("http")) {
-      return;
-    }
-    if (id.startsWith(".")) {
-      return;
-    }
-
-    // handle importMap
-    const importMap = await opts.getImportMap?.();
-    const importMapped = importMap && importMap.imports[id];
-    if (importMapped) {
-      return importMapped;
-    }
-
-    // fallback to skypack
-    const ret = `https://cdn.skypack.dev/${id}`;
-    opts.onWarn?.(`[http-resolver]: fallback ${id} to ${ret}`);
-    return ret;
-  };
 }
 
 export type CompileOptions = RollupOptions &
@@ -50,12 +21,12 @@ export function createCompilerOptionBuilder(
 ) {
   return async function (opts: CompileOptions) {
     const memfs = createMemoryFs(opts.files);
-    const importMap: ImportMap | undefined = opts.importMapPath
-      ? JSON.parse((await memfs.readFile(opts.importMapPath)).toString())
+    const importmaps: ImportMaps | undefined = opts.importMapsPath
+      ? JSON.parse((await memfs.readFile(opts.importMapsPath)).toString())
       : undefined;
     const { plugins } = configBuilder({
       fs: memfs,
-      importMap,
+      importmaps,
       ...opts,
     });
     return {
