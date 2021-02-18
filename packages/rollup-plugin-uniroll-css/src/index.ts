16da21d1136cacdadab4a2a4924f633a5386892d
RyosukeCla <RyosukeCla@users.noreply.github.com>
2021-02-18 16:59:22 +0900

    add output option to css plugin

--- packages/rollup-plugin-uniroll-css/src/index.ts
+++ packages/rollup-plugin-uniroll-css/src/index.ts
@@ -4,12 +4,34 @@ import { transformCss } from "./transformCss";
 import { wrapWithStyleInjector } from "./wrapWithInjector";
 
 export const css = (options: Options = {}) => {
+
+  const willOutputFile = !!options.output;
+  let cssFiles = Object.create(null);
+
   return {
+    name: 'css',
+    buildStart() {
+      cssFiles = {};
+    },
     async transform(code: string, id: string): Promise<string | void> {
-      if (id.endsWith(".css")) {
-        const css = await transformCss(code, options);
+      if (!id.endsWith('.css')) return;
+      const css = await transformCss(code, options);
+      
+      if (willOutputFile) {
+        cssFiles[id] = css;
+        return;
+      } else {
         return wrapWithStyleInjector(css);
       }
+    },
+    generateBundle() {
+      if (!willOutputFile) return;
+      if (cssFiles.length === 0) return;
+      this.emitFile({
+        type: "asset",
+        fileName: options.output,
+        source: cssFiles.join("")
+      });
     }
   } as Plugin;
 };
--- packages/rollup-plugin-uniroll-css/src/types.ts
+++ packages/rollup-plugin-uniroll-css/src/types.ts
@@ -1,3 +1,7 @@
 export type Options = {
   postprocess?: (css: string) => Promise<string>;
+  /**
+   * output css file instead of js injection.
+   */
+  output?: string;
 };
