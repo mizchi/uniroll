cee947f58d6525fc4806c264560f91a6672c44b5
harukitosa <13haruki28@gmail.com>
2021-03-11 10:42:51 +0900

    add test

--- packages/rollup-plugin-uniroll-svelte/test/svelte-plugin.test.ts
+++ packages/rollup-plugin-uniroll-svelte/test/svelte-plugin.test.ts
@@ -3,7 +3,7 @@ import fetch from "isomorphic-unfetch";
 import { bundle } from "uniroll";
 import { svelte } from "../src";
 import ts from "typescript";
-import { Plugin } from "rollup";
+import { Plugin, RollupWarning } from "rollup";
 import { createStylePreprocessor } from "../src/server/stylePreprocessor";
 import { css } from "rollup-plugin-uniroll-css";
 global.fetch = fetch;
@@ -63,6 +63,47 @@ new App({target: document.body});
 
   const out = await bundled.generate({ format: "es" });
   expect(out.output[0].code).toMatchSnapshot();
+});
+
+it("bundle svelte warnings check", async () => {
+  let warnings: RollupWarning[] = [];
+  const bundled = await bundle({
+    // resolveIdFallback: "https://cdn.skypack.dev/",
+    resolveIdFallback,
+    input: "/index.tsx",
+    files: {
+      "/index.tsx": `
+import App from "./App.svelte";
+new App({target: document.body});
+      `,
+      "/App.svelte": `
+<script lang="ts">
+  let x = 1;
+</script>
+<style>
+  .text {
+    color: red;
+  }
+</style>
+<span class="text">{z}</span> // z is not defined 
+      `,
+    },
+    rollupOptions: {
+      onwarn(warn) {
+        warnings.push(warn);
+      },
+    },
+    extraPlugins: [
+      svelte({
+        target: ts.ScriptTarget.ES2019,
+        resolveIdFallback,
+        svelteOptions: {},
+      }) as any,
+    ],
+  });
+  console.log(warnings);
+  expect(warnings.length).toBe(1);
+  expect(warnings[0].message).toBe("'z' is not defined (9:20)");
 });
 
 it("bundle for es5", async () => {
